b06bdb729c603d767d3fe56df0973c6dbd70eb6f
Richard Macklin <richard.github@nrm.com>
2021-01-22 16:31:21 -0800

    Allow fetch_through_merge_base.sh `--deepen` parameter to be customized
    By default, the script will run `git fetch --deepen=10 ...` in the loop
    to fetch more commits, but now this can be changed by setting the
    `DEEPEN_LENGTH` environment variable.

--- fetch_through_merge_base.sh
+++ fetch_through_merge_base.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
+DEEPEN_LENGTH=${DEEPEN_LENGTH:-10}
+
 git fetch --progress --depth=1 origin "+refs/heads/$BASE_REF:refs/heads/$BASE_REF"
 while [ -z "$( git merge-base "$BASE_REF" "$HEAD_REF" )" ]; do
-  git fetch -q --deepen=10 origin "$BASE_REF" "$HEAD_REF";
+  git fetch -q --deepen="$DEEPEN_LENGTH" origin "$BASE_REF" "$HEAD_REF";
 done
